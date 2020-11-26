9d3f4f90d1a92d34e93153743fc31a4a5d942a01
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-11-26 00:18:25 -0300

    + Documentação do seeder

--- src/seeders/Seeder.ts
+++ src/seeders/Seeder.ts
@@ -5,15 +5,16 @@ import seedAddresses from './AddressesSeeder';
 import  seedUsers  from './UsersSeeder';
 import seedProducts from './ProductsSeeder';
 
+// Seeda o bd com n elementos de cada tipo
 export default (req: Request, res: Response) => {
-    let n: number = +req.params['n'];
+    let n: number = +req.params['n'];   // Lê o valor de n
 
     try {
-        seedUsers(n);
-        seedAddresses(n);
-        seedProducts(n);
+        seedUsers(n);       // Seeda usuários
+        seedAddresses(n);   // Seeda endereços
+        seedProducts(n);    // Seeda produtos
     } catch(err) {
-        return res.status(500).json({"error": "Internal server error"})
+        return res.status(500).json({"error": "Internal server error"}) // Retorna um erro
     }
-    return res.status(200).json({"success": "database seeded"})
+    return res.status(200).json({"success": "database seeded"}) // Retorna menssagem de sucesso
 }
