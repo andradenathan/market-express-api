fb027cc24cfc8c66bb259666022d216732a35a21
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-11-25 23:34:25 -0300

    + Documentação do start

--- src/database/start.ts
+++ src/database/start.ts
@@ -1,13 +1,14 @@
 import { sequelize } from "./config";
 import { Request, Response } from 'express'; 
 
+// Inicia o banco de dados (Casos não esteja em produção)
 export default async (req: Request, res: Response) => {
-    if (process.env.NODE_ENV !== "development") {
+    if (process.env.NODE_ENV !== "development") {   // Verifica se o bd está em produção e retorna um erro caso esteja
         res.status(401).json({'error': "Can't restart DB in production"}).send;
     }
     try {
-        await sequelize.sync({ force: true });
-        await sequelize.authenticate();
+        await sequelize.sync({ force: true });  // Migra o db
+        await sequelize.authenticate(); // testa a conexão com o bd
         res.status(200).json('Connection has been established successfully').send;
     } catch (error) {
         res.status(400).json({'error': 'Unable to connect to the database'}).send;
