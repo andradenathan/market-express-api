267b6017ebef257eed569dad0e71bf971768e901
Gabriel Cunha <gabrielrcunha15@gmail.com>
2020-11-25 23:34:59 -0300

    + Documentação da middleware de oferta

--- src/middlewares/OfferMiddleware.ts
+++ src/middlewares/OfferMiddleware.ts
@@ -2,16 +2,17 @@ import { Product } from "../models/Product"
 import { Request, Response } from 'express'; 
 import { User } from "../models/User";
 
+// Valida as ofertas
 export default async (req: Request, res: Response, next: any) => {
     try {
-        const product = await Product.findByPk(req.params['product_id']);
-        const user = await User.findByPk(req.params['user_id']);
+        const product = await Product.findByPk(req.params['product_id']);   // Encontra o produto pelo id
+        const user = await User.findByPk(req.params['user_id']);   // Encontra o usuário pelo id
 
-        if (product?.ownerId === user?.id) {
+        if (product?.ownerId === user?.id) {    // Verifica se o usuário não está ofertando em um produto próprio
             return res.status(401).json({"error": "User can't make offer on their own product"});
         }
 
-        if (product && req.body.value <= product.value) {
+        if (product && req.body.value <= product.value) {   // Verifica se a oferta atual é mais alta que a anterior
             return res.status(401).json({"error": "Offer too low"});
         }
 
@@ -19,5 +20,5 @@ export default async (req: Request, res: Response, next: any) => {
         return res.status(500).json({"error": "Internal server error"});
     }
 
-    next();
+    next(); // Segue a execução caso não haja nenhum problema
 }
